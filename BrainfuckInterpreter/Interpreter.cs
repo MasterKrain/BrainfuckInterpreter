671c3830e400f2de6e97cdccac0635590d9f8247
Stan Graafmans <stan_live@hotmail.nl>
2017-04-22 13:02:36 +0200

    Initial commit

---
+++ BrainfuckInterpreter/Interpreter.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace BrainfuckInterpreter
+{
+    class Interpreter
+    {
+        private byte[] m_Code;
+
+        private string m_File;
+
+        private string m_Title = "Brainfuck interpreter by Stan Q. Graafmans, 2017";
+        public string Title { get { return m_Title; } }
+
+        private byte[] m_Cells;
+
+        private ushort m_Pointer = 0;
+        private ushort m_MemorySize = 0;
+
+        public Interpreter(ushort memorySize)
+        {
+            m_MemorySize = memorySize;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(m_Title);
+        }
+
+        private void Init()
+        {
+            m_Cells = new byte[m_MemorySize];
+
+            int length = m_Cells.Length;
+            for (int i = 0; i < length; ++i)
+            {
+                m_Cells[i] = 0;
+            }
+
+            m_Pointer = 0;
+        }
+
+        public void Interpret(string file)
+        {
+            Init();
+
+            Console.WriteLine();
+
+            ReadCode(file);
+
+            Stack<int> loopStack = new Stack<int>();
+
+            int length = m_Code.Length;
+            for (int i = 0; i < length; ++i)
+            {
+                if (loopStack.Count > 0)
+                {
+                    if (m_Code[i] != 91 && m_Code[i] != 93 && loopStack.Peek() < 0) continue;
+                }
+
+                switch (m_Code[i])
+                {
+                    case 60:
+                        m_Pointer--;
+                        continue;
+                    case 62:
+                        m_Pointer++;
+                        continue;
+                    case 44:
+                        m_Cells[m_Pointer] = (byte) Console.ReadKey().KeyChar;
+                        continue;
+                    case 46:
+                        Console.Write((char) m_Cells[m_Pointer]);
+                        continue;
+                    case 43:
+                        m_Cells[m_Pointer]++;
+                        continue;
+                    case 45:
+                        m_Cells[m_Pointer]--;
+                        continue;
+                    case 91:
+                        if (m_Cells[m_Pointer] > 0) loopStack.Push(i + 1);
+                        else loopStack.Push(-1);
+                        continue;
+                    case 93:
+                        if (loopStack.Count > 0)
+                        {
+                            if (loopStack.Peek() < 0)
+                            {
+                                loopStack.Pop();
+                                continue;
+                            }
+
+                            if (m_Cells[m_Pointer] > 0)
+                            {
+                                i = loopStack.Peek() - 1;
+                            }
+                            else
+                            {
+                                loopStack.Pop();
+                            }
+                        }
+
+                        continue;
+                }
+            }
+        }
+
+        private void ReadCode(string file)
+        {
+            m_File = file;
+
+            m_Code = File.ReadAllBytes(@file);
+        }
+    }
+}
---
+++ BrainfuckInterpreter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrainfuckInterpreter
+{
+    class Program
+    {
+        private static Interpreter m_Interpreter;
+
+        static void Main(string[] args)
+        {
+            m_Interpreter = new Interpreter(30000);
+            Console.Title = m_Interpreter.Title;
+            m_Interpreter.Interpret(args[0]);
+            Console.ReadKey();
+        }
+    }
+}
